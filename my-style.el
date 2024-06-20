ac9aa397e5b2e4bf16d7ae28a5902d06781f9872
Ray Kuo <ray20200720@gmail.com>
2024-06-20 11:12:14 +0800

    backup init.el before replace

--- my-style.el
+++ my-style.el
@@ -1,11 +1,18 @@
 (defun vscode-style ()
   (interactive)
+  (if (file-exists-p "~/.emacs.d/init.el")
+    (progn
+      (setq timestamp (format-time-string "%Y%m%d%H%M%S"))
+      (rename-file "~/.emacs.d/init.el" (concat "~/.emacs.d/init-" timestamp ".el"))))
   (copy-file "~/.emacs.d/vscode-init.el" "~/.emacs.d/init.el" t t)
   (restart-emacs))
 
 (defun windows-style ()
   (interactive)
+  (if (file-exists-p "~/.emacs.d/init.el")
+    (progn
+      (setq timestamp (format-time-string "%Y%m%d%H%M%S"))
+      (rename-file "~/.emacs.d/init.el" (concat "~/.emacs.d/init-" timestamp ".el"))))
   (copy-file "~/.emacs.d/windows-init.el" "~/.emacs.d/init.el" t t)
   (restart-emacs))
-
 
