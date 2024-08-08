acccf83065a72b004033e3ff4b279853b9d9b357
Ray Kuo <ray20200720@gmail.com>
2024-08-08 17:39:26 +0800

    add real-python-init.el & update my-style.el: add real-python-style & update setup.py: add real-python-init.el

--- my-style.el
+++ my-style.el
@@ -15,4 +15,13 @@
       (rename-file "~/.emacs.d/init.el" (concat "~/.emacs.d/init-" timestamp ".el"))))
   (copy-file "~/.emacs.d/windows-init.el" "~/.emacs.d/init.el" t t)
   (restart-emacs))
+	
+(defun real-python-style ()
+  (interactive)
+  (if (file-exists-p "~/.emacs.d/init.el")
+    (progn
+      (setq timestamp (format-time-string "%Y%m%d%H%M%S"))
+      (rename-file "~/.emacs.d/init.el" (concat "~/.emacs.d/init-" timestamp ".el"))))
+  (copy-file "~/.emacs.d/real-python-init.el" "~/.emacs.d/init.el" t t)
+  (restart-emacs))	
 
---
+++ real-python-init.el
@@ -0,0 +1,68 @@
+;; Reference: https://realpython.com/emacs-the-best-python-editor
+
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
+(require 'package)
+
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+;; Initializes the package infrastructure
+(package-initialize)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Installs packages
+;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults
+    material-theme
+    )
+  )
+
+(mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      myPackages)
+
+;; ===================================
+;; Basic Customization
+;; ===================================
+
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'material t)            ;; Load material theme
+(global-linum-mode t)               ;; Enable line numbers globally
+
+;; User-Defined init.el ends here
+
+
+;; ===================================
+;; My Configuration 
+;; ===================================
+(set-face-attribute 'default nil :height 110)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
+
+(setq-default cursor-type 'bar)
+
+(tool-bar-mode 0)
+
+(cond
+ ((eq system-type 'windows-nt)
+  (setq default-directory "D:/"))
+ ((eq system-type 'gnu/linux)
+  (setq default-directory "~/")))
+
+(cond
+ ((eq system-type 'windows-nt)
+  (when (member "JetBrains Mono" (font-family-list))
+    (set-frame-font "JetBrains Mono" t t)))
+ ((eq system-type 'gnu/linux)
+  (when (member "DejaVu Sans Mono" (font-family-list))
+    (set-frame-font "DejaVu Sans Mono" t t))))
